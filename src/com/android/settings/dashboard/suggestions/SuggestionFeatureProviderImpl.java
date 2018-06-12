8671bde359923bebf7db69303f20ec0914a1b56c
xyyx <xyyx@mail.ru>
2018-06-12 16:46:16 +0200

    Settings: Disable dashboard suggestions and conditions [2/3]
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- src/com/android/settings/dashboard/suggestions/SuggestionFeatureProviderImpl.java
+++ src/com/android/settings/dashboard/suggestions/SuggestionFeatureProviderImpl.java
@@ -20,6 +20,7 @@ import android.app.ActivityManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.provider.Settings;
 
 import androidx.annotation.NonNull;
 
@@ -60,6 +61,8 @@ public class SuggestionFeatureProviderImpl implements SuggestionFeatureProvider 
 
     @Override
     public boolean isSuggestionComplete(Context context, @NonNull ComponentName component) {
+        if (Settings.System.getInt(context.getContentResolver(),
+                Settings.System.SETTINGS_SHOW_SUGGESTIONS, 1) == 0) return true;
         final String className = component.getClassName();
         if (className.equals(WallpaperSuggestionActivity.class.getName())) {
             return WallpaperSuggestionActivity.isSuggestionComplete(context);
