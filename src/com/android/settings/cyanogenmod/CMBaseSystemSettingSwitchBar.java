AOKP/packages_apps_Settings

Settings - Add CM's preference files
Change-Id: I961ccf0be1c2d1514676ee11590ab01fe707495b

src/com/android/settings/cyanogenmod/CMBaseSystemSettingSwitchBar.java
@@ -1,0 +1,156 @@
+/*
+* Copyright (C) 2015 The CyanogenMod Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.settings.cyanogenmod;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Handler;
+import android.widget.Switch;
+import com.android.settings.widget.SwitchBar;
+
+import cyanogenmod.providers.CMSettings;
+
+public class CMBaseSystemSettingSwitchBar implements SwitchBar.OnSwitchChangeListener  {
+    private Context mContext;
+    private SwitchBar mSwitchBar;
+    private SettingsObserver mSettingsObserver;
+    private boolean mListeningToOnSwitchChange = false;
+
+    private boolean mStateMachineEvent;
+
+    private final String mSettingKey;
+    private final int mDefaultState;
+
+    private final SwitchBarChangeCallback mCallback;
+    public interface SwitchBarChangeCallback {
+        public void onEnablerChanged(boolean isEnabled);
+    }
+
+    public CMBaseSystemSettingSwitchBar(Context context, SwitchBar switchBar, String key,
+                                      boolean defaultState, SwitchBarChangeCallback callback) {
+        mContext = context;
+        mSwitchBar = switchBar;
+        mSettingKey = key;
+        mDefaultState = defaultState ? 1 : 0;
+        mCallback = callback;
+        mSettingsObserver = new SettingsObserver(new Handler());
+        setupSwitchBar();
+    }
+
+    public void setupSwitchBar() {
+        setSwitchState();
+        if (!mListeningToOnSwitchChange) {
+            mSwitchBar.addOnSwitchChangeListener(this);
+            mListeningToOnSwitchChange = true;
+        }
+        mSwitchBar.show();
+    }
+
+    public void teardownSwitchBar() {
+        if (mListeningToOnSwitchChange) {
+            mSwitchBar.removeOnSwitchChangeListener(this);
+            mListeningToOnSwitchChange = false;
+        }
+        mSwitchBar.hide();
+    }
+
+    public void resume(Context context) {
+        mContext = context;
+        if (!mListeningToOnSwitchChange) {
+            mSwitchBar.addOnSwitchChangeListener(this);
+            mSettingsObserver.observe();
+
+            mListeningToOnSwitchChange = true;
+        }
+    }
+
+    public void pause() {
+        if (mListeningToOnSwitchChange) {
+            mSwitchBar.removeOnSwitchChangeListener(this);
+            mSettingsObserver.unobserve();
+
+            mListeningToOnSwitchChange = false;
+        }
+    }
+
+    private void setSwitchBarChecked(boolean checked) {
+        mStateMachineEvent = true;
+        mSwitchBar.setChecked(checked);
+        mStateMachineEvent = false;
+        if (mCallback != null) {
+            mCallback.onEnablerChanged(checked);
+        }
+    }
+
+    private void setSwitchState() {
+        boolean enabled = CMSettings.System.getInt(mContext.getContentResolver(),
+                mSettingKey, mDefaultState) == 1;
+        mStateMachineEvent = true;
+        setSwitchBarChecked(enabled);
+        mStateMachineEvent = false;
+    }
+
+    @Override
+    public void onSwitchChanged(Switch switchView, boolean isChecked) {
+        //Do nothing if called as a result of a state machine event
+        if (mStateMachineEvent) {
+            return;
+        }
+
+        // Handle a switch change
+        CMSettings.System.putInt(mContext.getContentResolver(),
+                mSettingKey, isChecked ? 1 : 0);
+
+        if (mCallback != null) {
+            mCallback.onEnablerChanged(isChecked);
+        }
+    }
+
+    class SettingsObserver extends ContentObserver {
+        SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(CMSettings.System.getUriFor(
+                    mSettingKey), false, this);
+            update();
+        }
+
+        void unobserve() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.unregisterContentObserver(this);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            update();
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            update();
+        }
+
+        public void update() {
+            setSwitchState();
+        }
+    }
+}

src/com/android/settings/cyanogenmod/CMGlobalSettingSwitchPreference.java
@@ -1,0 +1,66 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.cyanogenmod;
+
+import android.content.Context;
+import android.preference.SwitchPreference;
+import android.util.AttributeSet;
+
+import cyanogenmod.providers.CMSettings;
+
+public class CMGlobalSettingSwitchPreference extends SwitchPreference {
+    public CMGlobalSettingSwitchPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public CMGlobalSettingSwitchPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CMGlobalSettingSwitchPreference(Context context) {
+        super(context, null);
+    }
+
+    @Override
+    protected boolean persistBoolean(boolean value) {
+        if (shouldPersist()) {
+            if (value == getPersistedBoolean(!value)) {
+                // It's already there, so the same as persisting
+                return true;
+            }
+            CMSettings.Global.putInt(getContext().getContentResolver(), getKey(), value ? 1 : 0);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected boolean getPersistedBoolean(boolean defaultReturnValue) {
+        if (!shouldPersist()) {
+            return defaultReturnValue;
+        }
+        return CMSettings.Global.getInt(getContext().getContentResolver(),
+                getKey(), defaultReturnValue ? 1 : 0) != 0;
+    }
+
+    @Override
+    protected boolean isPersisted() {
+        // Using getString instead of getInt so we can simply check for null
+        // instead of catching an exception. (All values are stored as strings.)
+        return CMSettings.Global.getString(getContext().getContentResolver(), getKey()) != null;
+    }
+}

src/com/android/settings/cyanogenmod/CMSecureSettingSwitchPreference.java
@@ -1,0 +1,66 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.cyanogenmod;
+
+import android.content.Context;
+import android.preference.SwitchPreference;
+import android.util.AttributeSet;
+
+import cyanogenmod.providers.CMSettings;
+
+public class CMSecureSettingSwitchPreference extends SwitchPreference {
+    public CMSecureSettingSwitchPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public CMSecureSettingSwitchPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CMSecureSettingSwitchPreference(Context context) {
+        super(context, null);
+    }
+
+    @Override
+    protected boolean persistBoolean(boolean value) {
+        if (shouldPersist()) {
+            if (value == getPersistedBoolean(!value)) {
+                // It's already there, so the same as persisting
+                return true;
+            }
+            CMSettings.Secure.putInt(getContext().getContentResolver(), getKey(), value ? 1 : 0);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected boolean getPersistedBoolean(boolean defaultReturnValue) {
+        if (!shouldPersist()) {
+            return defaultReturnValue;
+        }
+        return CMSettings.Secure.getInt(getContext().getContentResolver(),
+                getKey(), defaultReturnValue ? 1 : 0) != 0;
+    }
+
+    @Override
+    protected boolean isPersisted() {
+        // Using getString instead of getInt so we can simply check for null
+        // instead of catching an exception. (All values are stored as strings.)
+        return CMSettings.Secure.getString(getContext().getContentResolver(), getKey()) != null;
+    }
+}

src/com/android/settings/cyanogenmod/CMSystemSettingSwitchPreference.java
@@ -1,0 +1,66 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.cyanogenmod;
+
+import android.content.Context;
+import android.preference.SwitchPreference;
+import android.util.AttributeSet;
+
+import cyanogenmod.providers.CMSettings;
+
+public class CMSystemSettingSwitchPreference extends SwitchPreference {
+    public CMSystemSettingSwitchPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public CMSystemSettingSwitchPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CMSystemSettingSwitchPreference(Context context) {
+        super(context, null);
+    }
+
+    @Override
+    protected boolean persistBoolean(boolean value) {
+        if (shouldPersist()) {
+            if (value == getPersistedBoolean(!value)) {
+                // It's already there, so the same as persisting
+                return true;
+            }
+            CMSettings.System.putInt(getContext().getContentResolver(), getKey(), value ? 1 : 0);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected boolean getPersistedBoolean(boolean defaultReturnValue) {
+        if (!shouldPersist()) {
+            return defaultReturnValue;
+        }
+        return CMSettings.System.getInt(getContext().getContentResolver(),
+                getKey(), defaultReturnValue ? 1 : 0) != 0;
+    }
+
+    @Override
+    protected boolean isPersisted() {
+        // Using getString instead of getInt so we can simply check for null
+        // instead of catching an exception. (All values are stored as strings.)
+        return CMSettings.System.getString(getContext().getContentResolver(), getKey()) != null;
+    }
+}

Havoc-OS/android_packages_apps_Settings

src/com/android/settings/cyanogenmod/CMBaseSystemSettingSwitchBar.java
@@ -1,0 +1,156 @@
+/*
+* Copyright (C) 2015 The CyanogenMod Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.settings.cyanogenmod;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Handler;
+import android.widget.Switch;
+import com.android.settings.widget.SwitchBar;
+
+import lineageos.providers.LineageSettings;
+
+public class CMBaseSystemSettingSwitchBar implements SwitchBar.OnSwitchChangeListener  {
+    private Context mContext;
+    private SwitchBar mSwitchBar;
+    private SettingsObserver mSettingsObserver;
+    private boolean mListeningToOnSwitchChange = false;
+
+    private boolean mStateMachineEvent;
+
+    private final String mSettingKey;
+    private final int mDefaultState;
+
+    private final SwitchBarChangeCallback mCallback;
+    public interface SwitchBarChangeCallback {
+        public void onEnablerChanged(boolean isEnabled);
+    }
+
+    public CMBaseSystemSettingSwitchBar(Context context, SwitchBar switchBar, String key,
+                                      boolean defaultState, SwitchBarChangeCallback callback) {
+        mContext = context;
+        mSwitchBar = switchBar;
+        mSettingKey = key;
+        mDefaultState = defaultState ? 1 : 0;
+        mCallback = callback;
+        mSettingsObserver = new SettingsObserver(new Handler());
+        setupSwitchBar();
+    }
+
+    public void setupSwitchBar() {
+        setSwitchState();
+        if (!mListeningToOnSwitchChange) {
+            mSwitchBar.addOnSwitchChangeListener(this);
+            mListeningToOnSwitchChange = true;
+        }
+        mSwitchBar.show();
+    }
+
+    public void teardownSwitchBar() {
+        if (mListeningToOnSwitchChange) {
+            mSwitchBar.removeOnSwitchChangeListener(this);
+            mListeningToOnSwitchChange = false;
+        }
+        mSwitchBar.hide();
+    }
+
+    public void resume(Context context) {
+        mContext = context;
+        if (!mListeningToOnSwitchChange) {
+            mSwitchBar.addOnSwitchChangeListener(this);
+            mSettingsObserver.observe();
+
+            mListeningToOnSwitchChange = true;
+        }
+    }
+
+    public void pause() {
+        if (mListeningToOnSwitchChange) {
+            mSwitchBar.removeOnSwitchChangeListener(this);
+            mSettingsObserver.unobserve();
+
+            mListeningToOnSwitchChange = false;
+        }
+    }
+
+    private void setSwitchBarChecked(boolean checked) {
+        mStateMachineEvent = true;
+        mSwitchBar.setChecked(checked);
+        mStateMachineEvent = false;
+        if (mCallback != null) {
+            mCallback.onEnablerChanged(checked);
+        }
+    }
+
+    private void setSwitchState() {
+        boolean enabled = LineageSettings.System.getInt(mContext.getContentResolver(),
+                mSettingKey, mDefaultState) == 1;
+        mStateMachineEvent = true;
+        setSwitchBarChecked(enabled);
+        mStateMachineEvent = false;
+    }
+
+    @Override
+    public void onSwitchChanged(Switch switchView, boolean isChecked) {
+        //Do nothing if called as a result of a state machine event
+        if (mStateMachineEvent) {
+            return;
+        }
+
+        // Handle a switch change
+        LineageSettings.System.putInt(mContext.getContentResolver(),
+                mSettingKey, isChecked ? 1 : 0);
+
+        if (mCallback != null) {
+            mCallback.onEnablerChanged(isChecked);
+        }
+    }
+
+    class SettingsObserver extends ContentObserver {
+        SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(LineageSettings.System.getUriFor(
+                    mSettingKey), false, this);
+            update();
+        }
+
+        void unobserve() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.unregisterContentObserver(this);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            update();
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            update();
+        }
+
+        public void update() {
+            setSwitchState();
+        }
+    }
+}

src/com/android/settings/cyanogenmod/CMGlobalSettingSwitchPreference.java
@@ -1,0 +1,66 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.cyanogenmod;
+
+import android.content.Context;
+import android.support.v14.preference.SwitchPreference;
+import android.util.AttributeSet;
+
+import lineageos.providers.LineageSettings;
+
+public class CMGlobalSettingSwitchPreference extends SwitchPreference {
+    public CMGlobalSettingSwitchPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public CMGlobalSettingSwitchPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CMGlobalSettingSwitchPreference(Context context) {
+        super(context, null);
+    }
+
+    @Override
+    protected boolean persistBoolean(boolean value) {
+        if (shouldPersist()) {
+            if (value == getPersistedBoolean(!value)) {
+                // It's already there, so the same as persisting
+                return true;
+            }
+            LineageSettings.Global.putInt(getContext().getContentResolver(), getKey(), value ? 1 : 0);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected boolean getPersistedBoolean(boolean defaultReturnValue) {
+        if (!shouldPersist()) {
+            return defaultReturnValue;
+        }
+        return LineageSettings.Global.getInt(getContext().getContentResolver(),
+                getKey(), defaultReturnValue ? 1 : 0) != 0;
+    }
+
+    @Override
+    protected boolean isPersisted() {
+        // Using getString instead of getInt so we can simply check for null
+        // instead of catching an exception. (All values are stored as strings.)
+        return LineageSettings.Global.getString(getContext().getContentResolver(), getKey()) != null;
+    }
+}

src/com/android/settings/cyanogenmod/CMSecureSettingSwitchPreference.java
@@ -1,0 +1,66 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.cyanogenmod;
+
+import android.content.Context;
+import android.support.v14.preference.SwitchPreference;
+import android.util.AttributeSet;
+
+import lineageos.providers.LineageSettings;
+
+public class CMSecureSettingSwitchPreference extends SwitchPreference {
+    public CMSecureSettingSwitchPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public CMSecureSettingSwitchPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CMSecureSettingSwitchPreference(Context context) {
+        super(context, null);
+    }
+
+    @Override
+    protected boolean persistBoolean(boolean value) {
+        if (shouldPersist()) {
+            if (value == getPersistedBoolean(!value)) {
+                // It's already there, so the same as persisting
+                return true;
+            }
+            LineageSettings.Secure.putInt(getContext().getContentResolver(), getKey(), value ? 1 : 0);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected boolean getPersistedBoolean(boolean defaultReturnValue) {
+        if (!shouldPersist()) {
+            return defaultReturnValue;
+        }
+        return LineageSettings.Secure.getInt(getContext().getContentResolver(),
+                getKey(), defaultReturnValue ? 1 : 0) != 0;
+    }
+
+    @Override
+    protected boolean isPersisted() {
+        // Using getString instead of getInt so we can simply check for null
+        // instead of catching an exception. (All values are stored as strings.)
+        return LineageSettings.Secure.getString(getContext().getContentResolver(), getKey()) != null;
+    }
+}

src/com/android/settings/cyanogenmod/CMSystemSettingSwitchPreference.java
@@ -1,0 +1,66 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.cyanogenmod;
+
+import android.content.Context;
+import android.support.v14.preference.SwitchPreference;
+import android.util.AttributeSet;
+
+import lineageos.providers.LineageSettings;
+
+public class CMSystemSettingSwitchPreference extends SwitchPreference {
+    public CMSystemSettingSwitchPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public CMSystemSettingSwitchPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CMSystemSettingSwitchPreference(Context context) {
+        super(context, null);
+    }
+
+    @Override
+    protected boolean persistBoolean(boolean value) {
+        if (shouldPersist()) {
+            if (value == getPersistedBoolean(!value)) {
+                // It's already there, so the same as persisting
+                return true;
+            }
+            LineageSettings.System.putInt(getContext().getContentResolver(), getKey(), value ? 1 : 0);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected boolean getPersistedBoolean(boolean defaultReturnValue) {
+        if (!shouldPersist()) {
+            return defaultReturnValue;
+        }
+        return LineageSettings.System.getInt(getContext().getContentResolver(),
+                getKey(), defaultReturnValue ? 1 : 0) != 0;
+    }
+
+    @Override
+    protected boolean isPersisted() {
+        // Using getString instead of getInt so we can simply check for null
+        // instead of catching an exception. (All values are stored as strings.)
+        return LineageSettings.System.getString(getContext().getContentResolver(), getKey()) != null;
+    }
+}