f42682edefc92357bc79a8a112507d15999a3ffd
Louis <luisgonzales272@gmail.com>
2018-07-24 18:57:11 -0600

    Update SettingsActivity.java

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -714,6 +714,17 @@ public class SettingsActivity extends SettingsDrawerActivity
      */
     private Fragment switchToFragment(String fragmentName, Bundle args, boolean validate,
             boolean addToBackStack, int titleResId, CharSequence title, boolean withTransition) {
+        
+            if (ROOT_MANAGER_FRAGMENT.equals(fragmentName)) {
+            if (isRootAvailable()) {
+                Intent rootManagementIntent = new Intent();
+                rootManagementIntent.setClassName(mRootPackage, mRootClass);
+                startActivity(rootManagementIntent);
+                finish();
+                return null;
+            }
+}
+            
         if (validate && !isValidFragment(fragmentName)) {
             throw new IllegalArgumentException("Invalid fragment for this activity: "
                     + fragmentName);
@@ -845,7 +856,12 @@ public class SettingsActivity extends SettingsDrawerActivity
                 WifiDisplaySettings.isAvailable(this), isAdmin)
                 || somethingChanged;
 
-        if (UserHandle.MU_ENABLED && !isAdmin) {
+        // Root management
+        setTileEnabled(new ComponentName(packageName,
+                        Settings.RootManagementActivity.class.getName()),
+                isRootAvailable(), isAdmin);
+            
+      if (UserHandle.MU_ENABLED && !isAdmin) {
 
             // When on restricted users, disable all extra categories (but only the settings ones).
             final List<DashboardCategory> categories = mDashboardFeatureProvider.getAllCategories();
@@ -877,6 +893,43 @@ public class SettingsActivity extends SettingsDrawerActivity
         }
     }
 
+    
+    // Maximum available root managers
+    private int ROOT_MGR_MAX = 2; // mRootManagers
+    private Object[][] mRootManagers = {
+        {
+            "eu.chainfire.supersu", //pkg name
+            "eu.chainfire.supersu.MainActivity", // class name
+            185, // minimum version
+        },
+        {
+            "me.phh.superuser", //pkg name
+            "com.koushikdutta.superuser.MainActivity", // class name
+            0, // minimum version
+        },
+        {
+            "com.topjohnwu.magisk", //pkg name
+            "com.topjohnwu.magisk.SplashActivity", // class name
+            0,
+        },
+    };
+
+    private boolean isRootAvailable() {
+        mRootSupport = false;
+        mRootPackage = "";
+        mRootClass = "";
+        for (int i = 0; i <= ROOT_MGR_MAX; i++) {
+            try {
+                mRootSupport = getPackageManager().getPackageInfo((String) mRootManagers[i][0], 0).versionCode >= (int) mRootManagers[i][2];
+                mRootPackage = (String) mRootManagers[i][0];
+                mRootClass = (String) mRootManagers[i][1];
+                if (mRootSupport) return true;
+            } catch (PackageManager.NameNotFoundException e) {
+            }
+        }
+        return false;
+}
+                
     /**
      * @return whether or not the enabled state actually changed.
      */
