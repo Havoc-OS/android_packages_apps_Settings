d0c459536881fe892a4981b22bcef8ca72d99c52
Anushek Prasal <anushekprasal@gmail.com>
2018-11-06 21:05:00 +0530

    Ambient Play settings improvements
    Partially taken from https://github.com/PixelExperience/packages_apps_Settings/commit/06322d9b48d155a63d685948ad05fee9f77ddd75
    
    @SKULSHADY: Use Pie style switchbar

--- src/com/android/settings/AmbientPlaySettings.java
+++ src/com/android/settings/AmbientPlaySettings.java
@@ -16,21 +16,52 @@
 
 package com.android.settings;
 
-import android.content.Context;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.support.v4.content.ContextCompat;
+import android.support.v14.preference.SwitchPreference;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
-import com.android.settings.dashboard.DashboardFragment;
-import com.android.settings.havoc.AmbientPlayPreferenceController;
-import com.android.settings.havoc.AmbientPlayKeyguardPreferenceController;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
 
-public class AmbientPlaySettings extends DashboardFragment {
-    private static final String TAG = "AmbientPlaySettings";
+public class AmbientPlaySettings extends SettingsPreferenceFragment implements CompoundButton.OnCheckedChangeListener {
+
+    private TextView mTextView;
+    private View switchBar;
+
+    private SwitchPreference mAmbientRecognitionKeyguardPreference;
+    private SwitchPreference mAmbientRecognitionNotificationPreference;
+
+	private int mBackgroundActivatedColor;
+    private int mBackgroundColor;
+
+    private String AMBIENT_RECOGNITION_KEYGUARD = "ambient_recognition_keyguard";
+    private String AMBIENT_RECOGNITION_NOTIFICATION = "ambient_recognition_notification";
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        addPreferenceController(new AmbientPlayPreferenceController(context));
-        addPreferenceController(new AmbientPlayKeyguardPreferenceController(context));
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.ambient_play_settings);
+        mAmbientRecognitionKeyguardPreference = (SwitchPreference) findPreference(AMBIENT_RECOGNITION_KEYGUARD);
+        mAmbientRecognitionKeyguardPreference.setEnabled(isEnabled());
+        mAmbientRecognitionNotificationPreference = (SwitchPreference) findPreference(AMBIENT_RECOGNITION_NOTIFICATION);
+        mAmbientRecognitionNotificationPreference.setEnabled(isEnabled());
+		
+	    mBackgroundActivatedColor = ContextCompat.getColor(getActivity(), R.color.switchBarBackgroundActivatedColor);
+        mBackgroundColor = ContextCompat.getColor(getActivity(), R.color.switchBarBackgroundColor);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
     }
 
     @Override
@@ -38,13 +69,43 @@ public class AmbientPlaySettings extends DashboardFragment {
         return MetricsEvent.HAVOC_SETTINGS;
     }
 
-    @Override
-    protected String getLogTag() {
-        return TAG;
+    private boolean isEnabled() {
+        return Settings.System.getIntForUser(getActivity().getContentResolver(),
+                Settings.System.AMBIENT_RECOGNITION, 0, UserHandle.USER_CURRENT) != 0;
+    }
+
+    private void setEnabled(boolean enabled) {
+        Settings.System.putIntForUser(getActivity().getContentResolver(),
+                Settings.System.AMBIENT_RECOGNITION, enabled ? 1 : 0, UserHandle.USER_CURRENT);
     }
 
     @Override
-    protected int getPreferenceScreenResId() {
-        return R.xml.ambient_play_settings;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final View view = LayoutInflater.from(getActivity()).inflate(R.layout.ambient_play, container, false);
+        ((ViewGroup) view).addView(super.onCreateView(inflater, container, savedInstanceState));
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mTextView = view.findViewById(R.id.switch_text);
+        mTextView.setText(getString(isEnabled() ? R.string.ambient_play_switch_bar_on : R.string.ambient_play_switch_bar_off));
+        switchBar = view.findViewById(R.id.switch_bar);
+        switchBar.setBackgroundColor(isEnabled() ? mBackgroundActivatedColor : mBackgroundColor);
+        Switch switchWidget = switchBar.findViewById(android.R.id.switch_widget);
+        switchWidget.setChecked(isEnabled());
+        switchWidget.setOnCheckedChangeListener(this);
+        switchBar.setOnClickListener(v -> switchWidget.setChecked(!switchWidget.isChecked()));
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+        setEnabled(isChecked);
+        mTextView.setText(getString(isChecked ? R.string.ambient_play_switch_bar_on : R.string.ambient_play_switch_bar_off));
+	    switchBar.setBackgroundColor(isChecked ? mBackgroundActivatedColor : mBackgroundColor);
+        mAmbientRecognitionKeyguardPreference.setEnabled(isChecked);
+        mAmbientRecognitionNotificationPreference.setEnabled(isChecked);
     }
 }
