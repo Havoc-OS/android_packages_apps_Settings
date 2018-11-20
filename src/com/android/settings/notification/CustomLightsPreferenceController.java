b0b180d18605e88049433f4b0932043ccb0e4fd9
Vol Zhdanov <wight554@gmail.com>
2018-11-20 18:59:08 +0100

    Settings: Hide notification light color preference if no multicolor led
    on and off time will be still available instead

--- src/com/android/settings/notification/CustomLightsPreferenceController.java
+++ src/com/android/settings/notification/CustomLightsPreferenceController.java
@@ -59,7 +59,7 @@ public class CustomLightsPreferenceController extends NotificationPreferenceCont
         }
 
         return checkCanBeVisible(NotificationManager.IMPORTANCE_DEFAULT)
-                && canPulseLight();
+                && canPulseLight() && isMultiColorLed();
     }
 
     public void updateState(Preference preference) {
@@ -92,6 +92,11 @@ public class CustomLightsPreferenceController extends NotificationPreferenceCont
         }
         return Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.NOTIFICATION_LIGHT_PULSE, 1) == 1;
+    }
+
+    boolean isMultiColorLed() {
+        return mContext.getResources()
+                .getBoolean(com.android.internal.R.bool.config_multiColorBatteryLed);
     }
 
     private void showLedPreview() {
