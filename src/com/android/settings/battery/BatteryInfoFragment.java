d9ced87d7870ee97191b50369c295b9f23f79541
Anushek Prasal <anushekprasal@gmail.com>
2019-02-16 00:09:16 +0530

    Make advanced battery info opt-in

--- src/com/android/settings/battery/BatteryInfoFragment.java
+++ src/com/android/settings/battery/BatteryInfoFragment.java
@@ -40,6 +40,8 @@ public class BatteryInfoFragment extends DashboardFragment {
 
     private static final String LOG_TAG = "BatteryInfoFragment";
 
+    Context mContext;
+
     @Override
     public int getMetricsCategory() {
         return MetricsEvent.HAVOC_SETTINGS;
@@ -53,6 +55,11 @@ public class BatteryInfoFragment extends DashboardFragment {
     @Override
     protected int getPreferenceScreenResId() {
         return R.xml.battery_info;
+    }
+
+    public boolean isAvailable() {
+        return mContext.getResources().getBoolean(
+               R.bool.config_battery_info_available);
     }
 
     @Override
