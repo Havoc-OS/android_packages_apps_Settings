9372d820e1f756065783da055a1b513cadd7a8a0
ezio84 <brabus84@gmail.com>
2019-11-02 17:49:04 +0100

    Settings: Ambient Music Ticker - Allow to pulse on new tracks [2/2]
    Change-Id: Ie40955b574722782e01153a9f4434be296a8d5d3
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- src/com/android/settings/security/LockscreenDashboardFragment.java
+++ src/com/android/settings/security/LockscreenDashboardFragment.java
@@ -36,6 +36,7 @@ import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.display.AmbientDisplayAlwaysOnPreferenceController;
 import com.android.settings.display.AmbientDisplayNotificationsPreferenceController;
 import com.android.settings.display.AmbientWakeGesturesPreferenceController;
+import com.android.settings.display.PulseOnNewTracksPreferenceController;
 import com.android.settings.gestures.DoubleTapScreenPreferenceController;
 import com.android.settings.gestures.PickupGesturePreferenceController;
 import com.android.settings.gestures.ScreenOffUdfpsPreferenceController;
@@ -117,6 +118,7 @@ public class LockscreenDashboardFragment extends DashboardFragment
         use(PickupGesturePreferenceController.class).setConfig(getConfig(context));
         use(ScreenOffUdfpsPreferenceController.class).setConfig(getConfig(context));
         use(AmbientWakeGesturesPreferenceController.class).setConfig(getConfig(context));
+        use(PulseOnNewTracksPreferenceController.class).setConfig(getConfig(context));
 
         mControlsContentObserver = new ContentObserver(
                 new Handler(Looper.getMainLooper())) {
