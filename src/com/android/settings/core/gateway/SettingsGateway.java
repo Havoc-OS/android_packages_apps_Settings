f2207977f31298f92f276db439e9b0aef1440ab1
Jay Aliomer <aaliomer@google.com>
2019-12-10 14:49:27 -0500

    Settings: Dark theme settings activity
    Dark theme settings is now launchable as an activity
    
    Test: launch activity android.settings.DARK_THEME_SETTINGS
    Fixes: 145560830
    
    Change-Id: I24b8d3211e9d6e7a5b1f226001882573ae7fb311

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -75,6 +75,7 @@ import com.android.settings.deviceinfo.aboutphone.MyDeviceInfoFragment;
 import com.android.settings.deviceinfo.firmwareversion.FirmwareVersionSettings;
 import com.android.settings.deviceinfo.legal.ModuleLicensesDashboard;
 import com.android.settings.display.NightDisplaySettings;
+import com.android.settings.display.darkmode.DarkModeSettingsFragment;
 import com.android.settings.dream.DreamSettings;
 import com.android.settings.enterprise.EnterprisePrivacySettings;
 import com.android.settings.fuelgauge.AdvancedPowerUsageDetail;
@@ -291,6 +292,7 @@ public class SettingsGateway {
             ConfigCenter.class.getName(),
             GamingMode.class.getName(),
             RunningServices.class.getName(),
+            DarkModeSettingsFragment.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
