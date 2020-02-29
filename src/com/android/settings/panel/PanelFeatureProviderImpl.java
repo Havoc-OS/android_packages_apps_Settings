e7ad9f8e4c9e9654be5f1e84c01eb8c57259bf9e
Joe Maples <joe@maples.dev>
2020-02-29 15:40:36 -0500

    Settings: panels: Add Mobile Data panel
    Change-Id: Ifc698bc185ad4a0f4c97fc9ca948b7876d6f8724

--- src/com/android/settings/panel/PanelFeatureProviderImpl.java
+++ src/com/android/settings/panel/PanelFeatureProviderImpl.java
@@ -49,6 +49,8 @@ public class PanelFeatureProviderImpl implements PanelFeatureProvider {
                 return VolumePanel.create(context);
             case ACTION_MEDIA_OUTPUT_GROUP:
                 return MediaOutputGroupPanel.create(context, mediaPackageName);
+            case Settings.Panel.ACTION_MOBILE_DATA:
+                return MobileDataPanel.create(context);
         }
 
         throw new IllegalStateException("No matching panel for: "  + panelType);
