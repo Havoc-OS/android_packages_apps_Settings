2e362685df57551ac28f4411476c76781bbe4490
Anushek Prasal <anushekprasal@gmail.com>
2020-04-28 20:26:39 +0530

    Revert "Settings: Phone ringtone setting for Multi SIM device [2/3]"
    This reverts commit 2a05ff495d5d83500912b61e727bbb620c5f660a.

--- src/com/android/settings/notification/PhoneRingtonePreferenceController.java
+++ src/com/android/settings/notification/PhoneRingtonePreferenceController.java
@@ -19,37 +19,14 @@ package com.android.settings.notification;
 import android.content.Context;
 import android.media.RingtoneManager;
 
-import android.telephony.TelephonyManager;
-
-import androidx.preference.PreferenceScreen;
-
-import com.android.settings.DefaultRingtonePreference;
-import com.android.settings.R;
-
 import com.android.settings.Utils;
 
 public class PhoneRingtonePreferenceController extends RingtonePreferenceControllerBase {
 
-    private static final int SLOT_ID = 0;
     private static final String KEY_PHONE_RINGTONE = "ringtone";
 
     public PhoneRingtonePreferenceController(Context context) {
         super(context);
-    }
-
-    @Override
-    public void displayPreference(PreferenceScreen screen) {
-        super.displayPreference(screen);
-
-        TelephonyManager telephonyManager =
-                (TelephonyManager) mContext.getSystemService(Context.TELEPHONY_SERVICE);
-        if (telephonyManager.isMultiSimEnabled()) {
-            // For Multi SIM device, shoud show "Phone ringtone - SIM 1" for slot1 ringtone setting.
-            DefaultRingtonePreference ringtonePreference =
-                    (DefaultRingtonePreference) screen.findPreference(KEY_PHONE_RINGTONE);
-            ringtonePreference.setTitle(mContext.getString(R.string.ringtone1_title));
-            ringtonePreference.setEnabled(hasCard());
-        }
     }
 
     @Override
@@ -65,11 +42,5 @@ public class PhoneRingtonePreferenceController extends RingtonePreferenceControl
     @Override
     public int getRingtoneType() {
         return RingtoneManager.TYPE_RINGTONE;
-    }
-
-    private boolean hasCard() {
-        TelephonyManager telephonyManager =
-                (TelephonyManager) mContext.getSystemService(Context.TELEPHONY_SERVICE);
-        return telephonyManager.hasIccCard(SLOT_ID);
     }
 }
