77f13df748a78d10b141909e05ae799fa04309e6
Anushek Prasal <anushekprasal@gmail.com>
2023-04-01 05:06:49 +0530

    Revert "Settings: Add back increasing ring feature (2/2)."
    This reverts commit 33c26322372f5289710574d77767cee1eed5879f.
    
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- src/com/android/settings/notification/SoundSettings.java
+++ src/com/android/settings/notification/SoundSettings.java
@@ -63,8 +63,6 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
 
     @VisibleForTesting
     final VolumePreferenceCallback mVolumeCallback = new VolumePreferenceCallback();
-    private final IncreasingRingVolumePreferenceCallback mIncreasingRingVolumeCallback =
-            new IncreasingRingVolumePreferenceCallback();
     @VisibleForTesting
     final Handler mHandler = new Handler(Looper.getMainLooper()) {
         @Override
@@ -72,7 +70,6 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
             switch (msg.what) {
                 case STOP_SAMPLE:
                     mVolumeCallback.stopSample();
-                    mIncreasingRingVolumeCallback.stopSample();
                     break;
             }
         }
@@ -116,7 +113,6 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
     public void onPause() {
         super.onPause();
         mVolumeCallback.stopSample();
-        mIncreasingRingVolumeCallback.stopSample();
     }
 
     @Override
@@ -205,11 +201,6 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
             controller.setCallback(mVolumeCallback);
             getSettingsLifecycle().addObserver(controller);
         }
-
-        IncreasingRingVolumePreferenceController irvpc =
-                use(IncreasingRingVolumePreferenceController.class);
-        irvpc.setCallback(mIncreasingRingVolumeCallback);
-        getLifecycle().addObserver(irvpc);
     }
 
     // === Volumes ===
@@ -219,7 +210,6 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
 
         @Override
         public void onSampleStarting(SeekBarVolumizer sbv) {
-            mIncreasingRingVolumeCallback.stopSample();
             if (mCurrent != null) {
                 mHandler.removeMessages(STOP_SAMPLE);
                 mHandler.sendEmptyMessageDelayed(STOP_SAMPLE, SAMPLE_CUTOFF);
@@ -250,26 +240,6 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
         }
     }
 
-    final class IncreasingRingVolumePreferenceCallback implements
-            IncreasingRingVolumePreference.Callback {
-        private IncreasingRingVolumePreference mPlayingPref;
-
-        @Override
-        public void onSampleStarting(IncreasingRingVolumePreference pref) {
-            mPlayingPref = pref;
-            mVolumeCallback.stopSample();
-            mHandler.removeMessages(STOP_SAMPLE);
-            mHandler.sendEmptyMessageDelayed(STOP_SAMPLE, SAMPLE_CUTOFF);
-        }
-
-        public void stopSample() {
-            if (mPlayingPref != null) {
-                mPlayingPref.stopSample();
-                mPlayingPref = null;
-            }
-        }
-    };
-
     private static List<AbstractPreferenceController> buildPreferenceControllers(Context context,
             SoundSettings fragment, Lifecycle lifecycle) {
         final List<AbstractPreferenceController> controllers = new ArrayList<>();
@@ -280,8 +250,6 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
         controllers.add(new PhoneRingtonePreferenceController(context));
         controllers.add(new AlarmRingtonePreferenceController(context));
         controllers.add(new NotificationRingtonePreferenceController(context));
-        controllers.add(new IncreasingRingPreferenceController(context));
-        controllers.add(new IncreasingRingVolumePreferenceController(context));
 
         // === Other Sound Settings ===
         final DialPadTonePreferenceController dialPadTonePreferenceController =
