de9aa33971cb4ccf2ae233ad71f75034b7662144
Adin Kwok <adin.kwok@carbonrom.org>
2023-05-01 13:44:31 +0800

    Settings: Smart Pixels [2/2]
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

---
+++ src/com/android/settings/fuelgauge/batterysaver/SmartPixelsSettings.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2023 Havoc-OS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.fuelgauge.batterysaver;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class SmartPixelsSettings extends SettingsPreferenceFragment {
+
+    private static final String TAG = "SmartPixels";
+
+    private static final String SMART_PIXELS_FOOTER = "smart_pixels_footer";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.smart_pixels_settings);
+
+        findPreference(SMART_PIXELS_FOOTER).setTitle(R.string.smart_pixels_warning_text);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.HAVOC_SETTINGS;
+    }
+}
+
